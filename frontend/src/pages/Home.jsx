f5d7860b3bea352f9ed4f08abe34199f8dd4e43d
Rohit Verma <rohit55788888@gmail.com>
2026-02-20 15:01:24 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 
-import UserDashboard from "../components/userDashboard";
-import OwnerDashboard from "../components/ownerDashboard";
+import UserDashboard from "../components/UserDashboard";
+import OwnerDashboard from "../components/OwnerDashboard";
 import DeliveryBoy from "../components/DeliveryBoy";
 
 const Home = () => {
